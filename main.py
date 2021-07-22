e0b7e6d0efd6c951c4cca9ef64e6b58fb94e035d
Joshua Blewitt <joshua.blewitt@protonmail.com>
2021-07-22 22:22:32 +0100

    Able to connect to a DB and send a query!

---
+++ main.py
@@ -0,0 +1,88 @@
+# import libraries
+import psycopg2
+from psycopg2 import OperationalError
+import pandas as pd
+
+
+# main method
+def main():
+    """
+    The main method of the program
+    """
+    print('Welcome to Postgres Toolbox!')
+    connection = create_connection()
+    db_info = connection.info.dsn_parameters
+    print(f"Now connected to {db_info['dbname']} as {db_info['user']}")
+
+    while True:
+        print('Main Menu - enter the characters between the brackets to access the desired function')
+        print(f"(Q)uery {db_info['dbname']}")
+        print(f"(Exit) program and close connection to {db_info['dbname']}")
+
+        # get menu choice from user
+        menu_choice = input('Enter menu choice - ')
+
+        if menu_choice == 'Q':
+            db_query(connection)
+        elif menu_choice == 'Exit':
+            print('Goodbye!')
+            connection.close()
+            break
+        else:
+            print(f"I don't understand {menu_choice} - please try again!")
+
+
+def db_query(connection):
+    """
+    :param connection: Database connection
+    """
+    print(f"Query Database - {connection.info.dsn_parameters['dbname']}")
+    # Open a cursor to perform database operations
+    cursor = connection.cursor()
+    print('Enter your query below')
+    query = input('')
+    print('Now executing...')
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        print(result)
+        # close connection
+        cursor.close()
+    except OperationalError as e:
+        print(f"The error {e} occurred!")
+
+
+def create_connection():
+    """
+    :return: a connection object which can be used to interact with the Postgres DB
+    """
+
+    while True:
+        # get db info from user
+        print('Please enter the following information below.')
+        db_name = input('Enter the DB Name - ')
+        db_user = input('Enter the user name - ')
+        db_password = input('Enter the DB Password - ')
+        db_host = input('Enter the host name - ')
+        db_port = input('Enter the port - ')
+
+        try:
+            # attempt to connect to db
+            connection = psycopg2.connect(
+                database=db_name,
+                user=db_user,
+                password=db_password,
+                host=db_host,
+                port=db_port,
+            )
+            print(f"\nConnection to PostgreSQL DB successful")
+            return connection
+        except OperationalError as e:
+            # if it fails, throw error
+            print(f"The error '{e}' occurred")
+            print('Please try again!')
+
+
+# main entry point to program
+if __name__ == '__main__':
+    main()
